4d199f3a34057ce0c24782d45d424a14ac0c9296
Gulshan <gksept2005@gmail.com>
2024-04-17 16:16:52 +0530

    Multiple Movie Search threshhold 0.55

--- public/search.js
+++ public/search.js
@@ -23,10 +23,10 @@ function jaccard_similarity(bigrams1, bigrams2) {
 }
 
 async function searchMovies(searchQuery) {
-        
     let movies = await getMovies();
     let bigramSearchQuery = bigramize(searchQuery.toLowerCase());
 
+    let similarMovies = [];
     let maxSimilarity = 0;
     let mostSimilarMovie = null;
     let mostSimilarMovieIndex = -1;
@@ -35,6 +35,10 @@ async function searchMovies(searchQuery) {
         let bigramTitle = bigramize(movies[i].Title.toLowerCase());
         let similarity = jaccard_similarity(bigramSearchQuery, bigramTitle);
 
+        if (similarity > 0.55) {
+            similarMovies.push({ movie: movies[i], index: i });
+        }
+
         if (similarity > maxSimilarity) {
             maxSimilarity = similarity;
             mostSimilarMovie = movies[i];
@@ -42,37 +46,45 @@ async function searchMovies(searchQuery) {
         }
     }
 
-    let htmlMovie=`<div>You Mean <span style="color:green">${mostSimilarMovie.Title}</span></div>`
-    htmlMovie += `<div class="movie-container">
-        <div class="row mb-3 text-center">`;
-
-    if (mostSimilarMovie !== null) {
-        htmlMovie += ` 
-        <div class="card border-secondary col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
-            <img class="card-img-top mx-auto d-block m-2 rounded" src="${mostSimilarMovie.Image['280w']}" alt="${mostSimilarMovie.Title}">
-            <div class="card-body">
-                <h5 class="card-title">${mostSimilarMovie.Title}</h5>
-                <div class="card-text" style="text-align: left;">
-                    <h6>Duration:&nbsp;<small class="text-muted">${mostSimilarMovie.Metadata[1]}</small></h6>
-                    <h6>Rating:&nbsp;<small class="text-muted">${mostSimilarMovie.Rating[0]}/10</small></h6>
-                    <h6>Genre:&nbsp;<small class="text-muted">${mostSimilarMovie.genre.join(', ')}</small></h6>
-                    <h6>Director:&nbsp;<small class="text-muted">${mostSimilarMovie.Director.join(', ')}</small></h6>
-                    <h6>Stars:&nbsp;<small class="text-muted">${mostSimilarMovie.Cast.join(', ')}</small></h6>
-                </div>
-            </div>
-            <div class="card-footer">
-        <a href="/movie/${mostSimilarMovie.imdbID}?movieIndex=${mostSimilarMovieIndex}">
-            <button type="button" class="btn btn-secondary btn-sm" id="MoreBtn${mostSimilarMovieIndex}">More</button>
-        </a>
-            </div>
-        </div>
-        `;
+    let htmlMovie = '';
+    if (similarMovies.length > 0) {
+        htmlMovie += `<div>These movies might be what you're looking for:</div>`;
+        htmlMovie += `<div class="row mb-3 text-center">`
+        similarMovies.forEach(({ movie, index }) => {
+            htmlMovie += generateMovieCard(movie, index);
+        });
+    } else if (mostSimilarMovie !== null) {
+        htmlMovie += `<div>You might mean <span style="color:green">${mostSimilarMovie.Title}</span></div>`;
+        htmlMovie += `<div class="row mb-3 text-center">`
+        htmlMovie += generateMovieCard(mostSimilarMovie, mostSimilarMovieIndex);
     }
 
-    // Add the closing tags
-    htmlMovie += `</div></div>`;
-    document.querySelector('.pagination').innerHTML=''
-    document.querySelector('.otherThanNav').innerHTML=htmlMovie
+    htmlMovie += `</div>`
+    document.querySelector('.pagination').innerHTML = '';
+    document.querySelector('.otherThanNav').innerHTML = htmlMovie;
+}
+
+function generateMovieCard(movie, index) {
+    return `
+            <div class="card border-secondary col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
+                <img class="card-img-top mx-auto d-block m-2 rounded" src="${movie.Image['280w']}" alt="${movie.Title}">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.Title}</h5>
+                    <div class="card-text" style="text-align: left;">
+                        <h6>Duration:&nbsp;<small class="text-muted">${movie.Metadata[1]}</small></h6>
+                        <h6>Rating:&nbsp;<small class="text-muted">${movie.Rating[0]}/10</small></h6>
+                        <h6>Genre:&nbsp;<small class="text-muted">${movie.genre.join(', ')}</small></h6>
+                        <h6>Director:&nbsp;<small class="text-muted">${movie.Director.join(', ')}</small></h6>
+                        <h6>Stars:&nbsp;<small class="text-muted">${movie.Cast.join(', ')}</small></h6>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="/movie/${movie.imdbID}?movieIndex=${index}">
+                        <button type="button" class="btn btn-secondary btn-sm" id="MoreBtn${index}">More</button>
+                    </a>
+                </div>
+            </div>
+    `;
 }
 
 function handleSearch() {
